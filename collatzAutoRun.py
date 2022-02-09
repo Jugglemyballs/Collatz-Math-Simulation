021316ccbe362d33e95939d8ac204f83ee79aad9
Jugglemyballs <79810815+Jugglemyballs@users.noreply.github.com>
2022-02-08 23:48:27 -0800

    Update collatzAutoRun.py

--- collatzAutoRun.py
+++ collatzAutoRun.py
@@ -1,1 +1,35 @@
+if __name__ == '__main__':
+
+    def collatz_sequence(x):
+        seq = [x]
+
+        if x < 1:
+            return []
+
+        while x > 1:
+            if x % 2 == 0:
+                x = x / 2
+
+            elif x % 2 == 1:
+                x = x * 3 + 1
+            x = int(x)
+            seq.append(x)
+
+        return seq
+
+
+
+    number = 3 #starts testing at initial sequence of 2
+    z = collatz_sequence(number)
+
+
+    while z[-1] == 1:
+        number += 1 #tests next number if sequencing ends at 1
+        z = collatz_sequence(number)
+
+        print(z)
+
+        print('Ending Number: ' + str(z[-1]))
+
+        print('Length: ' + str(len(z)))
 
